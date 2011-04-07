f946a0fd64457d6b764aa278bd2a5a5de142349a
Toni Menzel <toni@okidokiteam.com>
2011-04-07 10:56:56 +0200

    resolves. Latest Exam version

--- src/test/java/org/qi4j/swoosh/Qi4JTest.java
+++ src/test/java/org/qi4j/swoosh/Qi4JTest.java
@@ -1,8 +1,8 @@
 package org.qi4j.swoosh;
 
 import org.junit.Test;
-import org.ops4j.pax.exam.swoosh.BundleCheck;
 import org.ops4j.pax.exam.swoosh.Player;
+import org.ops4j.pax.exam.swoosh.probes.CountBundles;
 
 import static org.ops4j.pax.exam.CoreOptions.*;
 
@@ -17,10 +17,15 @@ public class Qi4JTest {
     {
         String qi4jversion = "1.3.RC5";
 
+        System.setProperty( "org.ops4j.pax.url.mvn.repositories","+https://repository.jboss.org/nexus/content/repositories/thirdparty-releases@id=jboss" );
+
         Player player = new Player().with(
             composite(
                 mavenBundle().groupId( "org.apache.servicemix.bundles" ).artifactId( "org.apache.servicemix.bundles.asm" ).version( "3.3_1" ),
+                mavenBundle().groupId( "org.openrdf" ).artifactId( "openrdf-sesame-onejar-osgi" ).version( "2.1.1" ),
                 mavenBundle().groupId( "org.ops4j.pax.logging" ).artifactId( "pax-logging-api" ).version( "1.6.1" ),
+                mavenBundle().groupId( "org.ops4j.pax.logging" ).artifactId( "pax-logging-service" ).version( "1.6.1" ),
+
                 mavenBundle().groupId( "joda-time" ).artifactId( "joda-time" ).version( "1.6.2" ),
                 mavenBundle().groupId( "com.hazelcast" ).artifactId( "hazelcast" ).version( "1.9.2.1" ),
 
@@ -30,6 +35,8 @@ public class Qi4JTest {
                 mavenBundle().groupId( "org.qi4j.core" ).artifactId( "org.qi4j.core.runtime" ).version( qi4jversion ),
 
                 mavenBundle().groupId( "org.qi4j.library" ).artifactId( "org.qi4j.library.constraints" ).version( qi4jversion ),
+                mavenBundle().groupId( "org.qi4j.library" ).artifactId( "org.qi4j.library.fileconfig" ).version( qi4jversion ),
+
                 mavenBundle().groupId( "org.qi4j.library" ).artifactId( "org.qi4j.library.jmx" ).version( qi4jversion ),
                 mavenBundle().groupId( "org.qi4j.library" ).artifactId( "org.qi4j.library.locking" ).version( qi4jversion ),
                 mavenBundle().groupId( "org.qi4j.library" ).artifactId( "org.qi4j.library.rdf" ).version( qi4jversion ),
@@ -38,7 +45,9 @@ public class Qi4JTest {
                 mavenBundle().groupId( "org.qi4j.extension" ).artifactId( "org.qi4j.extension.entitystore-hazelcast" ).version( qi4jversion ),
 
                 //
-                mavenBundle().groupId( "org.apache.aries.blueprint" ).artifactId( "org.apache.aries.blueprint" ).version( "0.3.1" ).startLevel( 10 ),
+                mavenBundle().groupId( "org.apache.aries" ).artifactId( "org.apache.aries.util" ).version( "0.3" ),
+                mavenBundle().groupId( "org.apache.aries.proxy" ).artifactId( "org.apache.aries.proxy" ).version( "0.3" ),
+                mavenBundle().groupId( "org.apache.aries.blueprint" ).artifactId( "org.apache.aries.blueprint" ).version( "0.3.1" ),
 
                 mavenBundle().groupId( "org.apache.aries.jmx" ).artifactId( "org.apache.aries.jmx" ).version( "0.3" ),
                 mavenBundle().groupId( "org.apache.aries.jmx" ).artifactId( "org.apache.aries.jmx.blueprint" ).version( "0.3" ),
@@ -46,11 +55,14 @@ public class Qi4JTest {
                 mavenBundle().groupId( "org.apache.mina" ).artifactId( "mina-core" ).version( "2.0.0-RC1" ),
                 mavenBundle().groupId( "org.apache.sshd" ).artifactId( "sshd-core" ).version( "0.4.0" ),
                 mavenBundle().groupId( "org.apache.karaf.jaas" ).artifactId( "org.apache.karaf.jaas.config" ).version( "2.1.4" ),
-                mavenBundle().groupId( "org.apache.karaf.jaas" ).artifactId( "org.apache.karaf.jaas.modules" ).version( "2.1.4" )
+                mavenBundle().groupId( "org.apache.karaf.jaas" ).artifactId( "org.apache.karaf.jaas.modules" ).version( "2.1.4" ),
+                mavenBundle().groupId( "org.apache.karaf.jaas" ).artifactId( "org.apache.karaf.jaas.boot" ).version( "2.1.4" )
+
             )
         );
 
-        player.play( new BundleCheck().allResolved() );
+        
+        player.test( CountBundles.class,31).play();
     }
 
 }
